366aaaf895e67b572cac84d580cdd03b1d5254f7
Arnon Ruangthanawes <arnon.raungthanawes@unico.com.au>
2018-11-14 15:05:40 +1100

    Refresh properties

--- config-client-app/src/main/java/io/training/configclientapp/ConfigClientAppApplication.java
+++ config-client-app/src/main/java/io/training/configclientapp/ConfigClientAppApplication.java
@@ -5,17 +5,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class ConfigClientAppApplication {
 
     @Autowired
     private ConfigClientAppConfiguration properties;
 
+    // Is not automatically refresh when calling actuator/refresh
     @Value("${some.other.property}")
     private String someOtherPrperty;
 
