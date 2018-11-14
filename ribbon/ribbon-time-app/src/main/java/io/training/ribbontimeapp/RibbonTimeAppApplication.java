72b8179590664cec69f137c08486641b66fd0fe8
Arnon Ruangthanawes <arnon.raungthanawes@unico.com.au>
2018-11-15 10:27:49 +1100

    Client side load balancing

--- ribbon/ribbon-time-app/src/main/java/io/training/ribbontimeapp/RibbonTimeAppApplication.java
+++ ribbon/ribbon-time-app/src/main/java/io/training/ribbontimeapp/RibbonTimeAppApplication.java
@@ -4,15 +4,16 @@ import javax.inject.Inject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@EnableDiscoveryClient
+// @EnableDiscoveryClient
+@RibbonClient(name = "time-service") // client side load balancing
 @SpringBootApplication
 public class RibbonTimeAppApplication {
 
--- ribbon/ribbon-time-service/src/main/java/io/training/ribbontimeservice/RibbonTimeServiceApplication.java
+++ ribbon/ribbon-time-service/src/main/java/io/training/ribbontimeservice/RibbonTimeServiceApplication.java
@@ -5,12 +5,11 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableDiscoveryClient
+// @EnableDiscoveryClient
 @SpringBootApplication
 public class RibbonTimeServiceApplication {
 
